886164b7398cff86621734efa698d0706652042c
Abbiet274 <abigay4@clemson.edu>
2024-04-19 14:58:53 -0400

    hangman

--- GameRoom.py
+++ GameRoom.py
@@ -1,9 +1,155 @@
+
 from blackjack import play_blackjack
 from tictactoe import play_tictactoe
 
-user_game = input()
+# Defines custom exception class which handles any table not found errors.
+class ExitNotFoundError(Exception):
 
-if user_game == "b":
-    play_blackjack()
-elif user_game == "t":
-    play_tictactoe()
+    # Constructor with paramenters table_name and message with default argument "Table not found."
+    # Sets message and table_name within the class.
+    def __init__(self, table_name, message="Table not found"):
+        self.message = message
+        self.table_name = table_name
+    
+    # __str__ method returns the table name and message.
+    def __str__(self):
+        return f"{self.table_name} -> {self.message}"
+
+# Defines Table class which will be used to represent tables within the adventure map.
+class Table():
+
+    # Constructor with parameters name, description, and exits.
+    # Sets name, description, and exits within the class.
+    def __init__(self, name, description, exits):
+        self.name = name
+        self.description = description
+        self.exits = exits
+    
+    # Getter method returns table name.
+    def get_name(self):
+        return self.name
+
+    # Getter method returns table description.
+    def get_description(self):
+        return self.description
+
+    # Getter method returns table exits.
+    def get_exits(self):
+        return self.exits
+
+    # List exits method joins the exits in exits list with a newline and returns that string.
+    def list_exits(self):
+        return '\n'.join(self.exits)
+    
+    # __str__ method returns a string containg the name, description, and exits of the table.
+    def __str__(self):
+        return f"{self.name}: {self.description}\n\nExits:\n{self.list_exits()}"
+
+# Defines the AdventureMap class which manages the map of tables.
+class GameRoomMap():
+
+    # Contructor with no paramenters.
+    # Initializes empty map dictionary within the class.
+    def __init__(self):
+        self.map = {}
+    
+    # Add table method takes the table obeject as a paramenter.
+    # Adds the lowercase name of the table as the key and the table object as the value.
+    def add_table(self, table):
+        self.map[table.name.lower()] = table
+
+    # Getter method takes the table name as a paramenter.
+    # If the table exits in the map, returns the table object for that given name.
+    # If not, ExitNotFoundError is raised with table_name as the argument.
+    def get_table(self, table_name):
+        if table_name.lower() in self.map.keys():
+            return self.map[table_name.lower()]
+        else:
+            raise ExitNotFoundError(table_name)
+
+# Defines main program function.
+def main():
+
+    # Initializes AdventureMap instance.
+    game_room_map = GameRoomMap()
+
+    # Adds all tables in the house to the adventure map.
+    game_room_map.add_table(Table("Blackjack", "", ["Tic Tac Toe"]))
+    game_room_map.add_table(Table("Tic Tac Toe", "", ["Blackjack", "Hangman"]))
+    game_room_map.add_table(Table("Hangman", "", ["Tic Tac Toe", "Rock Paper Scissors"]))
+    game_room_map.add_table(Table("Rock Paper Scissors", "", ["Hangman", "Number Guesser"]))
+    game_room_map.add_table(Table("Number Guesser", "", ["Rock Paper Scissors", "Magic Squares"]))
+    game_room_map.add_table(Table("Magic Squares", "", ["Number Guesser", "Word Jumble"]))
+    game_room_map.add_table(Table("Word Jumble", "", ["Magic Squares"]))
+
+    # Welcomes user to Game Room.
+    print("\nWelcome to the Game Room! Starting at the Blackjack Table. To leave the Game Room, please type exit to jump out of the nearest window.\n")
+
+    # Initializes the current table (blackjack) and gets it from the game_room_map.
+    current_table = game_room_map.get_table("Blackjack")
+
+    # Displays the details of the current table.
+    print(current_table)
+
+    # Initialies exiting bool to False.
+    exiting = False
+
+    # Loops until the user wishes to exit the house.
+    while not exiting:
+
+        # Prompts user for exit choice.
+        print("Please choose an exit:")
+
+        # Sets users exit choice to user_exit, makes lowercase and strips leading trailing whitespace.
+        user_exit = input().lower().strip()
+
+        # Sets the available exits for the current table to exits.
+        exits = current_table.get_exits()
+
+        # If the user typed "exit," the while loop will stop, program terminates.
+        if user_exit == "exit":
+            
+            # Informs user that they are exiting the house.
+            print("Exiting the house out of the nearest window... thanks for the tour!")
+
+            # Sets exiting bool to True.
+            exiting = True
+        
+        elif user_exit.title() in exits:
+
+            # If the user's choice of exit is not available for the current table, except block runs.
+            try:
+
+                # Sets the given exit as the new current table.
+                current_table = game_room_map.get_table(user_exit)
+
+                # Outputs the details of the new current table.
+                print(current_table)
+
+                if user_exit.title() == "Blackjack":
+                    play_blackjack()
+                elif user_exit.title() == "Tic Tac Toe":
+                    play_tictactoe()
+
+                elif user_exit.title() == "Hangman":
+                elif user_exit.title() == "Rock Paper Scissors":
+                elif user_exit.title() == "Number Guesser":
+                elif user_exit.title() == "Magic Squares":
+                elif user_exit.title() == "Word Jumble":
+
+
+            
+            except ExitNotFoundError as e:
+                
+                # Handles table not found error using ExitNotFoundError class.
+                print(e)
+
+        # Runs if the table is not in the house at all.
+        else:
+
+            # Informs the user that the table was not found.
+            print(f"{user_exit} -> Table not found")
+
+# Runs main program function.                
+if __name__ == "__main__":
+    main()
---
+++ hangman.py
@@ -0,0 +1,214 @@
+
+import random # Imports the random module.
+
+def print_gallows(num_wrong_guesses: int) -> None:
+
+    """ Prints gallows depending on the number of wrong guesses.
+
+    Args: man_stages: Dict with the 7 different stages of the gallows (0-6 wrong guesses)
+    prints the key which corresponds to the number of wrong guesses the user has.
+    
+    Return: None.
+
+    """
+    str_wrong = str(num_wrong_guesses)
+
+    man_stages = {'0':
+    """
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========
+    """, '1':
+    """
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========
+    """, '2':
+    """
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========
+    """, '3':
+    """
+        +---+
+        |   |
+        O   |
+       /|   |
+            |
+            |
+        =========
+    """, '4':
+    """
+        +---+
+        |   |
+        O   |
+       /|\\  |
+            |
+            |
+        =========
+    """, '5':
+    """
+        +---+
+        |   |
+        O   |
+       /|\\  |
+       /    |
+            |
+        =========
+    """, '6':
+    """
+        +---+
+        |   |
+        O   |
+       /|\\  |
+       / \\  |
+            |
+        =========
+    """
+    }
+
+    print(man_stages[str_wrong])
+
+def choose_word(category: str) -> str:
+        
+    """ Chooses word(s) from chosen catagory.
+
+    Args: None.
+
+    Return: Random choice from the catagory given in catagories dict.
+
+    """
+    return random.choice(categories[category])
+
+def print_word(word: str, displayed_word: list, guess='-1') -> None:
+        
+    """ Prints the displayed word, updating if the guessed letter is in word.
+
+    Args: For loop interates len(word) times, setting displayed _word[i] to word[i] if the users guess was correct and
+    prints joined displayed word.
+    
+    Return: None.
+
+    """
+    for i in range(len(word)):
+        if word[i].lower() == guess:
+            displayed_word[i] = word[i]
+        
+    print(''.join(displayed_word))
+
+# Dict of catagories with words in each catagory.
+categories = {
+    'places': ['New York', 'Los Angeles', 'California', 'Chicago', 'Illinois',  
+        'Houston', 'Texas', 'Philadelphia', 'Pennsylvania', 'Phoenix', 'Arizona', 'San Antonio', 
+        'San Diego', 'Dallas', 'San Jose', 'Austin', 'Texas', 'Jacksonville', 'Florida', 
+        'San Francisco', 'Indianapolis', 'Indiana', 'Massachusetts', 'Mississippi'],
+    'movies': ["The Godfather", "The Shawshank Redemption", "Schindler's List",
+        "Raging Bull", "Casablanca", "Citizen Kane", "Gone With The Wind", "The Wizard Of Oz",
+        "Inception"],
+    "ta": ["Richard", "John", "Mars", "Aaron", "Brendan", "Jada", "Braylon"]
+}
+
+# Main program function.
+def main():
+
+    print("Please enter a seed: ") # Player is asked to input a seed.
+
+    user_seed = int(input()) # user_seed is set to the user's input.
+    random.seed(user_seed) # Random seed is set to players chosen seed.
+
+    # Welcomes player to hangman and tells them what to do.
+    print("Let's play Hangman!")
+    print("You are going to be given some empty dashes and you will guess the word in question by typing one letter at a time.")
+    print("Well... that is before you are hanged!\n")
+    
+    cat = random.choice(list(categories.keys())) # Chooses a random catagory from catagories, assigned to cat.
+    
+    print(f"The category is: {cat}\n") # Player is told their catagory.
+
+    word = choose_word(cat) # choose_word function run with chosen catagory, word assigned with word(s).
+    displayed_word = [] # Initializes empty list displayed_word.
+
+    # For loop iterates len(word) times and appends an "_" to displayed_word for each letter in word and a " " for each space.
+    for i in range(len(word)):
+
+        if word[i].isalpha():
+            displayed_word.append('_')
+        else:
+            displayed_word.append(' ')
+
+    
+    num_wrong_guesses = 0 # Initializes num_wrong_guesses variable.
+    guesses = [] # Initializes empty list guesses.
+
+    print_gallows(num_wrong_guesses) # print_gallows function ran with num_wrong_guesses = 0.
+    print_word(word, displayed_word) # print_word function ran with word and displayed_word as inputs.
+
+    playing = True # Initializes playing boolean to True.
+
+    # While loop iterates until playing = False.
+    while playing:
+
+        print("Guess a letter: ") # User is asked to guess a letter.
+        guess = input().strip().lower() # Users guess is stripped and turned all lowercase, assigned to guess variable.
+
+        # While loop iterates until user inputs a single letter that has not already been guessed, guess assigned as above.
+        while (len(guess) != 1) or (guess in guesses):
+            if (len(guess) != 1):
+                print("Please enter in just a singular letter")
+                guess = input().strip().lower()
+            else:
+                print("You have already guessed that letter!")
+                guess = input().strip().lower()
+
+        guesses.append(guess) # Adds user's guess to list of guesses.
+        
+        # If the user's guess is in word, the user is informed they are correct and the gallows and word(s) are printed with updated parameters.
+        if guess.lower() in word.lower():
+            print("Correct!")
+            print_gallows(num_wrong_guesses)
+            print_word(word, displayed_word, guess)
+        # Else, the user is informed that their guess is not in the word, num_wrong_guesses incremented.
+        else:
+            print(f"There are no {guess}'s")
+            num_wrong_guesses += 1
+            # If num_wrong_guesses is under 6, the game continues and the gallows and word(s) are printed with updated parameters.
+            if num_wrong_guesses < 6:
+                print_gallows(num_wrong_guesses)
+                print_word(word, displayed_word, guess)
+
+        # If the user guesses all of the letter in the word(s) the playing boolean is set to False, stopping the loop, and winner boolean is set to True.
+        if '_' not in displayed_word:
+            playing = False
+            winner = True
+
+        # If num_wrong_guesses is 6, the user used all their guesses, playing and winner booleans are set to False.
+        if num_wrong_guesses == 6:
+            playing = False
+            winner = False
+    
+    # If winner is true, user is informed that they won.
+    if winner:
+        print("You have won!")
+    # Else: the winner is informed that they lost, they are told the word, the gallows with full man is printed, and the displayed word is printed.
+    else:
+        print("Gave Over, you are HANGED!")
+        print(f"The word was: {word}")
+        print("Better luck next time.")
+        print_gallows(num_wrong_guesses)
+        print_word(word, displayed_word, guess)
+
+# Runs main.
+if __name__ == "__main__":
+    main()
